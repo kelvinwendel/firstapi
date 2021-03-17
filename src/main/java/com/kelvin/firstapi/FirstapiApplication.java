7dff2380190388598e02382c9e47e5b0faa69fc1
Kelvin Wendel <kelvin_wendel@hotmail.com>
2021-03-17 00:24:22 -0300

    Criação Estrutura básica da aplicação
    - Criada a estrutura básica da API spring boot.

---
+++ src/main/java/com/kelvin/firstapi/FirstapiApplication.java
@@ -0,0 +1,21 @@
+package com.kelvin.firstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe responsável pela inicialização da aplicação.
+ */
+@SpringBootApplication
+public class FirstapiApplication {
+
+	/**
+	 * Inicializa a aplicação.
+	 * 
+	 * @param args
+	 *   Parâmetros de inicialização da aplicação.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapiApplication.class, args);
+	}
+}
